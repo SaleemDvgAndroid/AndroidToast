c4aab1cfaf4991ad9057046c5b6c1783b5ac3b41
SaleemDvgAndroid <45421343+SaleemDvgAndroid@users.noreply.github.com>
2018-11-28 15:43:28 +0530

    Add files via upload

---
+++ src/android/AndroidToaster.java
@@ -0,0 +1,37 @@
+package toast.android;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.widget.Toast;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class AndroidToaster extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("showToast")) {
+            String message = args.getString(0);
+			if(message != null){
+			this.showToast(message, callbackContext);
+            return true;
+			}
+        }
+        return false;
+    }
+
+    private void showToast(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+			Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+}
